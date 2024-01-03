a2304bbbe131d2517325c10f66eb4832cd83e30b
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-03 16:03:01 +0100

    Add vpc module

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -13,3 +13,23 @@ locals {
   vpc_name = "tf-galaxy-vpc-zsolt"
   cluster_name = "tf-galaxy-cluster-zsolt"
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.4.0"
+
+  name = local.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
