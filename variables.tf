c07a63515c663a17e818028fdaf14f24c96e81b1
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-03 13:31:36 +0100

    Add helm release name variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type = string
   default = "eu-central-1"
 }
+
+variable "helm_release_name" {
+  description = "Name of the Helm release"
+  type = string
+  default = "tf-galaxy"
+}
